107150e5d317d65da0eef384fd7aef127c5e6ee1
yyi <yiyi@thsupcom.com>
2020-12-11 17:16:16 +0800

    创建项目

---
+++ src/main/java/org/yiyi/spring/iocstart/DefListBeanFactoryStarter.java
@@ -0,0 +1,58 @@
+/*
+ *
+ *
+ * Copyright ( c ) 2020 TH Supcom Corporation. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of TH Supcom
+ * Corporation ("Confidential Information").  You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with TH Supcom Corporation or a TH Supcom
+ * authorized reseller (the "License Agreement"). TH Supcom may make changes to the
+ * Confidential Information from time to time. Such Confidential Information may
+ * contain errors.
+ *
+ * EXCEPT AS EXPLICITLY SET FORTH IN THE LICENSE AGREEMENT, TH Supcom DISCLAIMS ALL
+ * WARRANTIES, COVENANTS, REPRESENTATIONS, INDEMNITIES, AND GUARANTEES WITH
+ * RESPECT TO SOFTWARE AND DOCUMENTATION, WHETHER EXPRESS OR IMPLIED, WRITTEN OR
+ * ORAL, STATUTORY OR OTHERWISE INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A
+ * PARTICULAR PURPOSE. TH Supcom DOES NOT WARRANT THAT END USER'S USE OF THE
+ * SOFTWARE WILL BE UNINTERRUPTED, ERROR FREE OR SECURE.
+ *
+ * TH Supcom SHALL NOT BE LIABLE TO END USER, OR ANY OTHER PERSON, CORPORATION OR
+ * ENTITY FOR INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL
+ * DAMAGES, OR DAMAGES FOR LOSS OF PROFITS, REVENUE, DATA OR USE, WHETHER IN AN
+ * ACTION IN CONTRACT, TORT OR OTHERWISE, EVEN IF TH Supcom HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES. TH Supcom' TOTAL LIABILITY TO END USER SHALL NOT
+ * EXCEED THE AMOUNTS PAID FOR THE TH Supcom SOFTWARE BY END USER DURING THE PRIOR
+ * TWELVE (12) MONTHS FROM THE DATE IN WHICH THE CLAIM AROSE.  BECAUSE SOME
+ * STATES OR JURISDICTIONS DO NOT ALLOW LIMITATION OR EXCLUSION OF CONSEQUENTIAL
+ * OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO END USER.
+ *
+ * Copyright version 2.0
+ */
+package org.yiyi.spring.iocstart;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.yiyi.spring.iocstart.entity.People;
+
+/**
+ * 编程式启动一个bean factory，从xml读取配置信息
+ * @author yi.yi
+ * @date 2020.12.04
+ */
+public class DefListBeanFactoryStarter
+{
+    public static void main (String[] args)
+    {
+        ClassPathResource cpr = new ClassPathResource ("META-INF/application.xml");
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory ();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader (factory);
+        reader.loadBeanDefinitions (cpr);
+
+        People people = (People)factory.getBean ("people");
+        System.out.println (people);
+    }
+}
---
+++ src/main/java/org/yiyi/spring/iocstart/entity/People.java
@@ -0,0 +1,73 @@
+/*
+ *
+ *
+ * Copyright ( c ) 2020 TH Supcom Corporation. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of TH Supcom
+ * Corporation ("Confidential Information").  You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with TH Supcom Corporation or a TH Supcom
+ * authorized reseller (the "License Agreement"). TH Supcom may make changes to the
+ * Confidential Information from time to time. Such Confidential Information may
+ * contain errors.
+ *
+ * EXCEPT AS EXPLICITLY SET FORTH IN THE LICENSE AGREEMENT, TH Supcom DISCLAIMS ALL
+ * WARRANTIES, COVENANTS, REPRESENTATIONS, INDEMNITIES, AND GUARANTEES WITH
+ * RESPECT TO SOFTWARE AND DOCUMENTATION, WHETHER EXPRESS OR IMPLIED, WRITTEN OR
+ * ORAL, STATUTORY OR OTHERWISE INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A
+ * PARTICULAR PURPOSE. TH Supcom DOES NOT WARRANT THAT END USER'S USE OF THE
+ * SOFTWARE WILL BE UNINTERRUPTED, ERROR FREE OR SECURE.
+ *
+ * TH Supcom SHALL NOT BE LIABLE TO END USER, OR ANY OTHER PERSON, CORPORATION OR
+ * ENTITY FOR INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL
+ * DAMAGES, OR DAMAGES FOR LOSS OF PROFITS, REVENUE, DATA OR USE, WHETHER IN AN
+ * ACTION IN CONTRACT, TORT OR OTHERWISE, EVEN IF TH Supcom HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES. TH Supcom' TOTAL LIABILITY TO END USER SHALL NOT
+ * EXCEED THE AMOUNTS PAID FOR THE TH Supcom SOFTWARE BY END USER DURING THE PRIOR
+ * TWELVE (12) MONTHS FROM THE DATE IN WHICH THE CLAIM AROSE.  BECAUSE SOME
+ * STATES OR JURISDICTIONS DO NOT ALLOW LIMITATION OR EXCLUSION OF CONSEQUENTIAL
+ * OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO END USER.
+ *
+ * Copyright version 2.0
+ */
+package org.yiyi.spring.iocstart.entity;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * @author yi.yi
+ * @date 2020.12.04
+ */
+public class People
+{
+    private String id;
+    private String name;
+
+    public String getId ()
+    {
+        return id;
+    }
+
+    public void setId (String id)
+    {
+        this.id = id;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString ()
+    {
+        return ToStringBuilder.reflectionToString (this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
